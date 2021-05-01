5dd684aa7bf407ce728e1ca69a9b3b7e38d0482b
Melanie DeWitt <melaniebd1986@gmail.com>
2021-05-01 16:13:27 -0600

    updating API to allow adding new games

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# Project Name
+# Steam More-Like-This
 
-> Project description
+> This service displays related games based on the tags each game is assigned so users can browse games that are more likely to suit their interests
 
 ## Related Projects
+  You can find the updated versions of this project here:
+  - https://github.com/rpt26-sdc-prototype
 
+  You can find original version of this project here:
   - https://github.com/rpt26-fec-pathfinder/anthony-photo-gallery-service
   - https://github.com/rpt26-fec-pathfinder/james-metadata-service
   - https://github.com/rpt26-fec-pathfinder/calvin-more-like-this-service
@@ -16,7 +19,9 @@
 1. [Development](#development)
 
 ## Usage
+Updated usage:
 
+Original usage:
 > Assuming you have webpack and mongo on your computer already.
 > If you are using Mongo, you will need to change the path in mongoose.connect() to your personal path. File is in database/index.js.
 > Run seed script by typing 'npm run seed' in terminal
@@ -41,4 +46,38 @@ npm install -g webpack
 npm install -g mongodb
 npm install
 ```
+## API
+See the web sequence diagram here:
+https://www.websequencediagrams.com/files/render?link=UsLeSPKAIkX15x3pasLFXnhy9t1ge6LgJJVQybFFDuUHUvOUPlPkVp5gjPThaXG5
 
+GET /:id
+Send HTML file for that game based on ID
+
+GET /morelikethis/:id
+Send array of similar games' data
+If no game data can be acquired, default text is sent
+
+{
+  id: integer,
+  title: string,
+  price: integer,
+  releaseDate: string,
+  reviewCount: integer,
+  reviewRating: string,
+  headerImage: string,
+  gallery: [strings]
+}
+
+Note that if data is unavailable, price will be a string
+
+POST /morelikethis
+Used by a service that handles adding new games to the Steam shop
+Expected data: JSON object
+
+{
+  id: integer,
+  tags: [strings],
+  similarGames: [integers]
+}
+
+Send back success or error
--- server/helper.js
+++ server/helper.js
@@ -15,34 +15,41 @@ const getData = async (page) => {
 
   // GET request to James' product endpoint
   const getProductInfo = () => {
+    console.log(new Date().toUTCString(), 'getProductInfo called');
     return axios.get(`http://3.227.255.185/api/product/${page}`);
   };
   // GET request to Tim's review endpoint
   const getReviewInfo = () => {
+    console.log(new Date().toUTCString(), 'getReviewInfo called');
     return axios.get(`http://18.144.23.11:4052/reviews/${page}`);
   };
   // GET request to Anthony's photo endpoint
   const getPhotoInfo = () => {
+    console.log(new Date().toUTCString(), 'getPhotoInfo called');
     return axios.get(`http://100.24.35.141:4012/images/${page}`);
   };
 
   await Promise.allSettled([getProductInfo(), getReviewInfo(), getPhotoInfo()])
     .then(results => {
+      console.log(new Date().toUTCString(), 'promise all settled')
       // console.log('this is results', results);
       results.forEach(result => {
         // if promise is rejected fill with placeholder data
         if (result.status === 'rejected') {
           data.id = page;
           if (result.reason.config.url.includes('3.227.255.185')) {
+            console.log(new Date().toUTCString(), 'if includes ip address');
             data.title = `Game ID ${page} Title`;
             data.price = `Game ID ${page} Price`;
             data.releaseDate = `Game ID ${page} Release Date`;
 
           } else if (result.reason.config.url.includes('4052')) {
+            console.log(new Date().toUTCString(), 'elseif includes 4052');
             data.reviewCount = `Game ID ${page} Review Count`;
             data.reviewRating = `Game ID ${page} Review Summary`;
 
           } else if (result.reason.config.url.includes('4012')) {
+            console.log(new Date().toUTCString(), 'elseif includes 4012');
             data.headerImage = `Game ID ${page} Header Image`;
             data.gallery = `Game ID ${page} Gallery`;
 
@@ -51,15 +58,16 @@ const getData = async (page) => {
           }
         } else {
           // if promise resolves, set equal to team data
+          console.log(new Date().toUTCString(), 'promise resolves')
           data.id = page;
           if (result.value.config.url.includes('3.227.255.185')) {
-
+            console.log(new Date().toUTCString(), 'resolve includes ip address');
             data.title = result.value.data.name;
             data.price = result.value.data.price;
             data.releaseDate = result.value.data.releaseDate;
 
           } else if (result.value.config.url.includes('4052')) {
-
+            console.log(new Date().toUTCString(), 'resolved else if includes 4052')
             data.reviewCount = result.value.data.length;
             let recommended = 0;
             for (let i = 0; i < data.reviewCount; i++) {
@@ -85,7 +93,7 @@ const getData = async (page) => {
             }
 
           } else if (result.value.config.url.includes('4012')) {
-
+            console.log(new Date().toUTCString(), 'else if resolved includes 4012')
             data.headerImage = result.value.data[0].headerImage;
             let gallery = result.value.data[0].mainImages.map(image => {
               return image.main;
@@ -101,4 +109,10 @@ const getData = async (page) => {
   return data;
 };
 
-module.exports = getData;
+const newEntry = (tags) => {
+  // call database to look for similar entries based on tags
+  // create an array of @10 other games with similar tags
+  // return array
+}
+
+module.exports = {getData, newEntry};
